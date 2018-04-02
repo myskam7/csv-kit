8014183df47329c14c1fb35a75e291d1152d7b15
neodc <neodc@localhost.localdomain>
2018-04-01 20:09:18 -0400

    added function constructor and dependencies

--- script.js
+++ script.js
@@ -3,8 +3,22 @@
     var csv = require('fast-csv');
     var multer = require('multer');
 
-    var kit = {
-        upload: multer({ dest: 'uploads/' })
-    };
+    function kit(filename) {
+        return new kit.init(filename);
+    }
 
+    kit.prototype = {};
+
+    kit.init = function(filename) {
+        var self = this;
+
+        self.filename = filename;
+
+        console.log('file to be parsed: ', self.filename);
+        
+    }
+
+    kit.init.prototype = kit.prototype;
+
+    module.exports = kit;
 }).call(this);
